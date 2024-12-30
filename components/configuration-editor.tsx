e85015f9bae64ee83fc1c983f584bcc7ebb361f8
xlncbasit <xlncbasit@gmail.com>
2024-12-30 10:13:20 +0530

    new

--- components/configuration-editor.tsx
+++ components/configuration-editor.tsx
@@ -33,9 +33,6 @@ export function ConfigurationEditor() {
         }
 
         const response = await fetch(`/api/load-config?org_key=${org_key}&module_key=${module_key}`, {
-          headers: {
-            'Accept': 'application/json'
-          }
         });
         
         if (!response.ok) {
